8554c601e6f0f552b763f17dbf0d458c0cfce843
DimiaD <dima.zozulia08@gmail.com>
2017-12-09 13:35:19 +0200

    Add files via upload

--- vgfile.sh
+++ vgfile.sh
@@ -18,16 +18,19 @@ chmod 400 'Ubuntu.pem'
 ls -l
 #chown ubuntu:users /tmp/UbuntuF.pem
 #chmod 400 '/tmp/UbuntuF.pem'
-pwd
 echo "=================docker build=================="
+cd /tmp/Docker
+pwd
 docker build -t dima .
 pwd
 echo "=================docker run===================="
+cd /tmp/Docker
+pwd
 docker run -p 7777:8090 -d dima
 
 echo "================ssh============================"
 cd /tmp
 pwd
-ssh -i 'Ubuntu.pem' -v -f -N -R 9999:localhost:7777 ubuntu@13.56.207.23
+ssh -i 'Ubuntu.pem' -v -f -N -R 9999:localhost:7777 ubuntu@13.56.207.23 >> /dev/null
 
 
